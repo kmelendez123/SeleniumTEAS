360da940b0e2848c4a8e4b7200b0248a154eaa38
kmelendez123 <kmelendez@stsiinc.com>
2017-08-24 13:20:46 -0400

    initial commit with working rspec tests

---
+++ spec/spec_helper.rb
@@ -0,0 +1,20 @@
+require 'selenium-webdriver'
+
+RSpec.configure do |config|
+
+  config.before(:each) do
+    case ENV['browser']
+    when 'firefox'
+      geckodriver = File.join(Dir.pwd, 'vendor', 'geckodriver')
+      @driver = Selenium::WebDriver.for :firefox, driver_path: geckodriver
+    when 'chrome'
+      chromedriver = File.join(Dir.pwd, 'vendor', 'chromedriver')
+      @driver = Selenium::WebDriver.for :chrome, driver_path: chromedriver
+    end
+  end
+
+  config.after(:each) do
+    @driver.quit
+  end
+
+end
